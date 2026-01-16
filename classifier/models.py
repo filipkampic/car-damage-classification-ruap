15f6db244b5d31727cf515b6fa511e22d027a7f4
Filip Kampić <filipkampic1@gmail.com>
2026-01-16 15:39:56 +0100

    Add prediction model and initial migration

--- classifier/models.py
+++ classifier/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Prediction(models.Model):
+    image = models.ImageField(upload_to='uploads/')
+    prediction = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.prediction} ({self.created_at})"
+
